b103ac5ec19eb35f427ab09e8b00143216b19ce2
sahya1697 <sahya1697@gmail.com>
2026-07-02 11:15:03 +0000

    comments removed

--- sample_c_files/main.c
+++ sample_c_files/main.c
@@ -1,5 +1,5 @@
 #include "shared.h"
-/* Rule 12.5 violation: operands of && are not primary-expressions */
+
 int check_limits(int x, int y) {
     if (x > 0 && y < 100 && x != y) { return 1; }
     return 0; }
--- sample_c_files/shared.h
+++ sample_c_files/shared.h
@@ -1,6 +1,6 @@
 #ifndef SHARED_H
 #define SHARED_H
-/* Rule 18.4 violation: unions shall not be used */
+
 union DataStore { unsigned int raw; unsigned char bytes[4]; };
 int check_limits(int x, int y);
 void reset_device(void);
